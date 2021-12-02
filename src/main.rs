6d16e251a081e54e4a356899e9ad8034573f13c0
Olivier Pelhatre <44462129+Ouam74@users.noreply.github.com>
2021-12-02 20:42:58 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 #![allow(unused)]
 
 use cpp_core::{Ptr, StaticUpcast};
-use qt_core::{q_init_resource, qs, slot, CheckState, QBox, QObject, QPtr, QPointF, SlotNoArgs, QFlags, AlignmentFlag, QCoreApplication};
+use qt_core::{
+    q_init_resource, qs, slot, CheckState, QBox, QObject, QPtr, QPointF, SlotNoArgs, QFlags,
+    AlignmentFlag, QCoreApplication, ApplicationAttribute, q_event_loop::ProcessEventsFlag
+};
 use qt_ui_tools::ui_form;
 use qt_gui::{QPen, QBrush, QColor, q_painter::RenderHint};
 use qt_widgets::{QApplication, QPushButton, QWidget, QFrame, QVBoxLayout};
@@ -60,6 +63,7 @@ impl TodoWidget {
        self.chart.create_default_axes();
        self.chart.set_animation_options(QFlags::from(AnimationOption::NoAnimation));
        self.chart.set_background_brush(&QBrush::from_q_color(&QColor::from_rgb_3a(0, 0, 0)));
+        // self.chart.set_background_brush(&QBrush::from_q_color(&QColor::from_rgb_3a(255, 255, 255)));
 
        self.chartview.set_chart(&self.chart);
        self.chartview.set_window_title(&qs("Charts example"));
@@ -68,10 +72,13 @@ impl TodoWidget {
        self.chartview.set_render_hint_1a(RenderHint::HighQualityAntialiasing);
 
        let pen = QPen::new();
-       pen.set_width_f(0.5);
+       pen.set_width_f(1.0);
+       // pen.set_color(&QColor::from_rgb_3a(0, 205, 255));
        pen.set_color(&QColor::from_rgb_3a(0, 255, 0));
        self.series.set_pen(&pen);
+       self.series.set_use_opengl_0a();
 
+       // let xAxis = QValueAxis::new_0a();
        self.series.attach_axis(&self.xaxis);
        self.chart.set_axis_x_2a(&self.xaxis, &self.series);
        self.xaxis.set_range(0 as f64, 10000 as f64);
@@ -92,28 +99,39 @@ impl TodoWidget {
         let sender = thread::spawn(move || { // no GUI stuffs here !!
             loop { // for _ in 0..1000 {
                let mut rng = rand::thread_rng();
-               let list = QListOfQPointF::new();
-               list.reserve(N as i32);
-               for i in 0..N as usize {
-                   let point = QPointF::new_2a(i as f64, rng.gen());
-                   list.append_q_point_f(&point);
+               let mut tx_arr: Vec<(f64,f64)> = Vec::with_capacity(N as usize); // let mut tx_arr = vec![vec![0.0f64; 2]; _ncol as usize];
+               for i in 0..N as usize { // (0..=1000).map(|x| x as f64 / 1.0).map(|x| (x, rng.gen()))
+                  tx_arr.push((i as f64, rng.gen())); // tx_arr[i][0] = i as f64; tx_arr[i][1] = rng.gen();
                }
-               tx.send(list).unwrap();
-               list.clear();
+               tx.send(tx_arr).unwrap(); // tx.send(tx_arr).expect("Unable to send on channel");
+               // thread::sleep(Duration::from_millis(10)); // we need to wait here, in order to give time to the rx to retrieve the data !! if not the consumed RAM increases.
+               // thread::sleep(Duration::from_micros(10000));
             }
         });
 
+        // let now = Instant::now();
         'rx: for received in rx { // The receiver lives in the main Thread, GUI stuffs are performed here
             if *self.abort.lock().unwrap() == 0 {
-                println!("{:?}", rx);
-                // self.xaxis.set_range(0 as f64, N as f64);
-                // self.series.replace_q_list_of_q_point_f(&rx);
+                // let now = Instant::now();
+                self.xaxis.set_range(0 as f64, N as f64);
+                let list = QListOfQPointF::new();
+                list.reserve(N as i32);
+                for j in 0..N as usize {
+                    // let point = QPointF::new_2a(received[j].0 / N as f64, received[j].1); //let point = QPointF::new_2a(received[j][0] / _ncol as f64, received[j][1]);
+                    let point = QPointF::new_2a(received[j].0, received[j].1);
+                    list.append_q_point_f(&point);
+                }
+                self.series.replace_q_list_of_q_point_f(&list);
+                list.clear();
                 QCoreApplication::process_events_0a();
+                // QCoreApplication::process_events_1a(QFlags::from(ProcessEventsFlag::EventLoopExec));
             }
             else {
                 break 'rx;
             }
        }
+       // let elapsed = now.elapsed();
+       // println!("Elapsed: {:?}", elapsed);
        sender.join();
     }
 
@@ -134,6 +152,9 @@ fn main() {
     QApplication::init(|_| {
         let todo_widget = TodoWidget::new();
         todo_widget.show();
-        unsafe { QApplication::exec() }
+        unsafe {
+            QCoreApplication::set_attribute_1a(ApplicationAttribute::AAUseOpenGLES);
+            QApplication::exec()
+        }
     })
 }
